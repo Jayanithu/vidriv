ede51d39b8f8c97fd03d13b82ab71898d8a71b64
Jayanithu <103057141+Jayanithu@users.noreply.github.com>
2025-05-16 04:37:47 +0530

    fix: remove unnecessary newline at end of file

--- types/drive.ts
+++ types/drive.ts
@@ -1,9 +1,9 @@
-export interface VideoFile {
+export interface DriveFile {
   id: string;
   name: string;
   mimeType: string;
   thumbnailLink?: string;
   webContentLink?: string;
   size?: string;
-  createdTime: string;
+  createdTime?: string;
 }
