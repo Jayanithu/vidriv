38c17423fa52ce0c0aa998bdf90093081cc4cc3d
Jayanithu <103057141+Jayanithu@users.noreply.github.com>
2025-05-15 09:44:24 +0530

    chore: update next-auth types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,15 +3,8 @@ import "next-auth";
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
-    user: {
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
   }
-}
-
-declare module "next-auth/jwt" {
+  
   interface JWT {
     accessToken?: string;
     refreshToken?: string;
